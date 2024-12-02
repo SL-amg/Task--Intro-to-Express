bd07c2ff18e3d9c275c240d4ef03fe42fcb068f1
Abdullah Abbas <sl_amg@msn.com>
2024-12-02 19:35:37 +0300

    added app.get to open link

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require("express");
 
 const app = express();
 
-app.listen(8000, () => {
+const port = 8000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+
+app.listen(port, () => {
     console.log("this is my first BackEnd Server and it is online");
   });
